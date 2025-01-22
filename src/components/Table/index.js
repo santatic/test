af3c8f26f68b7669f8498338eb1a488e887b6d21
Santa <namns.dev@gmail.com>
2025-01-22 18:20:26 +0700

    2. Validate Recipient Addresses from CSV File

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,7 +5,10 @@ import Pagination from "react-bootstrap/Pagination";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Papa from "papaparse";
+import { ethers } from "ethers";
+
 import "./style.css";
 
 const SenderTable = (props) => {
@@ -15,6 +18,7 @@ const SenderTable = (props) => {
   const { wallets, setWallets, isConnected } = props;
   const { currentPage, setCurrentPage } = useState(1);
   const [itemPerPage] = useState(5);
+  const inputFile = useRef(null) 
 
   useEffect(() => {
     indexOfLastItem = currentPage * itemPerPage;
@@ -25,13 +29,52 @@ const SenderTable = (props) => {
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+  const handleUpload = () => {
+    inputFile.current.click();
+  };
   const uploadWallet = async (e) => {
     // setWallets(dummy);
-    const response = await fetch(process.env.PUBLIC_URL + "/wallets.csv");
-    const data = await response.text();
-    const dataArray = data.replace(/\s/g, "").split(",");
-    const resultArr = dataArray.filter((item) => item !== "");
-    setWallets(resultArr);
+    // const response = await fetch(process.env.PUBLIC_URL + "/wallets.csv");
+    // const data = await response.text();
+    // const dataArray = data.replace(/\s/g, "").split(",");
+    // const resultArr = dataArray.filter((item) => item !== "");
+    // setWallets(resultArr);
+
+    const file = e.target.files[0];
+    const addresses = [];
+    const duplicate = [];
+    const invalid = [];
+
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        delimiter: ",",
+        skipEmptyLines: true,
+        step: function (row) {
+          const address = row.data.address;
+          if (!ethers.isAddress(address)) {
+            invalid.push(address);
+            return;
+          }
+          if (addresses.includes(address)) {
+            duplicate.push(address);
+            return;
+          }
+          addresses.push(address);
+        },
+        complete: () => {
+          setWallets(addresses);
+
+          if (duplicate.length > 0) {
+            alert(`Duplicate addresses: ${duplicate.join(", ")}`);
+          }
+
+          if (invalid.length > 0) {
+            alert(`Invalid addresses: ${invalid.join(", ")}`);
+          }
+        },
+      });
+    }
   };
 
   return (
@@ -75,10 +118,16 @@ const SenderTable = (props) => {
       </Pagination> */}
 
       <div className="tableButton">
+        <input type='file'
+          id='file'
+          ref={inputFile}
+          style={{display: 'none'}}
+          onChange={uploadWallet}
+        />
         <Button
           className="uploadButton"
           disabled={!isConnected}
-          onClick={uploadWallet}
+          onClick={handleUpload}
         >
           Upload file
         </Button>
